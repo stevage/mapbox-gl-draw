ae9ef80bade721c5bb0fced6d4a2be004efc3d82
makkoli <makkoli@vt.edu>
2021-02-11 10:52:23 -0500

    check if a ring is complete before modifying it

--- src/lib/create_polygon_from_partial_ring.js
+++ src/lib/create_polygon_from_partial_ring.js
@@ -1,4 +1,8 @@
 module.exports = function(ring) {
+  if (ring[0][0] === ring[ring.length - 1][0] && ring[0][1] === ring[ring.length - 1][1]) {
+    return [ring];
+  }
+
   const polygon = ring
     .slice()
     .map(coord => coord)
